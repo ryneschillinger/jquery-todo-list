5fd0839353dcaf18a9c18968d0ff383aaffa7b4a
Ryne Schillinger <ryneschillinger@gmail.com>
2016-12-06 09:15:13 -0800

    v3

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 	function addNewTask() {
 		var inputText = $("#input-text").val();
-		var taskRow = $("<div class='task-row'><input type='checkbox' class='checkbox' ><h4>" + inputText + "</h5><button class='delete'>DELETE</button></div>");
+		var taskRow = $("<div class='task-row'><input type='checkbox' class='checkbox' ><h4>" + inputText + "</h4><button class='delete'>DELETE</button></div>");
 		$("#task-list").append(taskRow.clone());
 	}
 
@@ -19,7 +19,7 @@ $(document).ready(function() {
 		$(this).parent().remove();
 	});
 
-	// Checking off a task
+	// Crossing off a task
 
 	$(document).on("click", ".checkbox", function() {
 		$(this).parent().find("h4").toggleClass("completed");			
